d646997896734889054ba3c4b9147ab651f8b35e
Alek Novkovski <alek@greatcode2.com>
2023-01-21 02:38:20 +0100

    Define basic homepage route

--- working/app.js
+++ working/app.js
@@ -7,6 +7,12 @@ const app = express();
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
+// routes
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
 
