5f382f78f13b3f01f55a1ba2ea1211b34b13f9b5
Alek Novkovski <alek@greatcode2.com>
2023-01-21 23:49:52 +0100

    Refactor, use a queryObject in the .find method

--- working/controllers/products.js
+++ working/controllers/products.js
@@ -5,8 +5,17 @@ async function getAllProductsStatic (req, res) {
     res.status(200).json({message: 'products testing route', products});
 }
 
+function queryObject(query, properties) {
+    const queryObject = {}
+    properties.map(property => {
+        query[property] ? queryObject[property]=query[property] : null
+    })
+    return queryObject
+}
+
 async function getAllProducts (req, res) {
-    const products = await Product.find(req.query);
+    const properties = ['featured', 'company', 'name']
+    const products = await Product.find(queryObject(req.query, properties));
     res.status(200).json({message: 'products route', products})
 }
 
